80cd5c828494560327f2cd49d497727cbe7f9d3e
Ronald Ponferrada <ronaldponferrada@Ronalds-2023-MacBook-Pro.local>
2023-08-03 08:19:17 -0700

    updated doe programs section

---
+++ assets/js/doe-programs.js
@@ -0,0 +1,18 @@
+let path = document.querySelector('.svg-container path')
+let pathLength = path.getTotalLength();
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+path.style.strokeDashOffset = pathLength;
+
+if (window.innerWidth > 767) {
+    window.addEventListener('scroll', () => {
+        // What % down is it?
+        var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+        // Length to offset the dashes
+        var drawLength = pathLength * scrollPercentage;
+        // Draw in reverse
+        path.style.strokeDashoffset = pathLength - drawLength;
+        
+    });
+}
