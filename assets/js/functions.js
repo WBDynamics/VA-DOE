33612d3c7ffcce7fb375c4cbcd8df116dc816e57
Ronald Ponferrada <ronaldponferrada@Ronalds-2023-MacBook-Pro.local>
2023-08-04 10:17:52 -0700

    updated doe programs files

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -58,3 +58,16 @@ $('.scroll').on('click',function(e) {
 $(function() {
 	$('.lazy').Lazy();
 });
+
+
+$('.vha-video__play-action').magnificPopup({
+    type: 'iframe', 
+    
+    iframe: {
+       markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                  '<div class="mfp-title">Some caption</div>'+
+                '</div>'
+    }
+});
