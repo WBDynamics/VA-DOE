e5a75d604c51bdeeb93aea4ccc41961028f244c0
Ronald Ponferrada <ronaldponferrada@Ronalds-2023-MacBook-Pro.local>
2023-08-08 10:54:00 -0400

    updated formula for scroll animation

--- _site/assets/js/doe-programs.js
+++ _site/assets/js/doe-programs.js
@@ -8,11 +8,19 @@ path.style.strokeDashOffset = pathLength;
 
 if (window.innerWidth > 767) {
     window.addEventListener('scroll', () => {
+
+        // Add div heights above the section
+        var section_above = $('section.above--svg-anim').height();
+
+        // Add div heights below the section
+        var section_below = $('section.below--svg-anim').height();
+
         // What % down is it?
-        var test_div = $('.test-div').height();
-        var scrollPercentage = ((-1 * test_div) + document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight + (-1 * test_div));
+        var scrollPercentage = ((-1 * section_above) + document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight + (-1 * section_above) + (-1 * section_below) + -300);
+
         // Length to offset the dashes
         var drawLength = pathLength * scrollPercentage;
+
         // Draw in reverse
         path.style.strokeDashoffset = pathLength - drawLength;
         
