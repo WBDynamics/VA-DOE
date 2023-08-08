815fc16c71cc873547af80062ff270a783a371a4
Ronald Ponferrada <ronaldponferrada@Ronalds-2023-MacBook-Pro.local>
2023-08-07 22:58:31 -0400

    updated scroll start for svg animation

--- assets/js/doe-programs.js
+++ assets/js/doe-programs.js
@@ -1,3 +1,4 @@
+
 let path = document.querySelector('.doe-svg-container path')
 let pathLength = path.getTotalLength();
 
@@ -8,12 +9,16 @@ path.style.strokeDashOffset = pathLength;
 if (window.innerWidth > 767) {
     window.addEventListener('scroll', () => {
         // What % down is it?
-        var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+        var test_div = $('.test-div').height();
+        var scrollPercentage = ((-1 * test_div) + document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight + (-1 * test_div));
         // Length to offset the dashes
         var drawLength = pathLength * scrollPercentage;
         // Draw in reverse
         path.style.strokeDashoffset = pathLength - drawLength;
         
     });
+    
 }
+
+
   
